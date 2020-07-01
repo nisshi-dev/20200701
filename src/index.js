774ed399d2d215a5d60d235ac7551f72391a28e0
nishidayoshikatsu <nsd24technology@gmail.com>
2020-07-01 15:26:15 +0900

    initial commit

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import "./styles.css";
+
+const canvas = document.querySelector("#draw-area");
+const context = canvas.getContext("2d");
+
+canvas.addEventListener("mousemove", event => {
+  draw(event.layerX, event.layerY);
+});
+canvas.addEventListener("touchmove", event => {
+  draw(event.layerX, event.layerY);
+});
+
+canvas.addEventListener("mousedown", () => {
+  context.beginPath();
+  isDrag = true;
+});
+canvas.addEventListener("mouseup", () => {
+  context.closePath();
+  isDrag = false;
+});
+canvas.addEventListener("touchstart", () => {
+  context.beginPath();
+  isDrag = true;
+});
+canvas.addEventListener("touchend", () => {
+  context.closePath();
+  isDrag = false;
+});
+
+const clearButton = document.querySelector("#clear-button");
+clearButton.addEventListener("click", () => {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+});
+
+let isDrag = false;
+function draw(x, y) {
+  if (!isDrag) {
+    return;
+  }
+  context.lineWidth = 5;
+  context.strokeStyle = "#7F5435"; // 発展1
+  context.lineTo(x, y);
+  context.stroke();
+}
